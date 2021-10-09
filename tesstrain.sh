cc219c752575d1d6cb221d5f350efac2aab367b2
Marcel-003 <marcel003@binus.ac.id>
2021-10-09 16:10:13 +0700

    fix tesstrain

--- tesstrain.sh
+++ tesstrain.sh
@@ -23,6 +23,7 @@
 #    --langdata_dir DATADIR     # Path to tesseract/training/langdata directory.
 #    --output_dir OUTPUTDIR     # Location of output traineddata file.
 #    --overwrite                # Safe to overwrite files in output_dir.
+#    --linedata_only            # Only generate training data for lstmtraining.
 #    --run_shape_clustering     # Run shape clustering (use for Indic langs).
 #    --exposures EXPOSURES      # A list of exposure levels to use (e.g. "-1 0 1").
 #
@@ -31,6 +32,8 @@
 #    --training_text TEXTFILE   # Text to render and use for training.
 #    --wordlist WORDFILE        # Word list for the language ordered by
 #                               # decreasing frequency.
+#    --textlist FONTS           # A list of filenames for text to render.
+#                               # Works like fontlist. Overrides training_text.
 #
 # OPTIONAL flag to specify location of existing traineddata files, required
 # during feature extraction. If unspecified will use TESSDATA_PREFIX defined in
@@ -42,31 +45,36 @@
 # fontconfig. An easy way to list the canonical names of all fonts available on
 # your system is to run text2image with --list_available_fonts and the
 # appropriate --fonts_dir path.
-
-
-source `dirname $0`/tesstrain_utils.sh
-
+ 
+ 
+source "$(dirname $0)/tesstrain_utils.sh"
+ 
 ARGV=("$@")
 parse_flags
-
+ 
 mkdir -p ${TRAINING_DIR}
 tlog "\n=== Starting training for language '${LANG_CODE}'"
-
-source `dirname $0`/language-specific.sh
+ 
+source "$(dirname $0)/language-specific.sh"
 set_lang_specific_parameters ${LANG_CODE}
-
+ 
 initialize_fontconfig
-
+ 
 phase_I_generate_image 8
 phase_UP_generate_unicharset
-phase_D_generate_dawg
-phase_E_extract_features "box.train" 8
-phase_C_cluster_prototypes "${TRAINING_DIR}/${LANG_CODE}.normproto"
-if [[ "${ENABLE_SHAPE_CLUSTERING}" == "y" ]]; then
-    phase_S_cluster_shapes
+if ((LINEDATA)); then
+  phase_E_extract_features "lstm.train" 8 "lstmf"
+  make__lstmdata
+else
+  phase_D_generate_dawg
+  phase_E_extract_features "box.train" 8 "tr"
+  phase_C_cluster_prototypes "${TRAINING_DIR}/${LANG_CODE}.normproto"
+  if [[ "${ENABLE_SHAPE_CLUSTERING}" == "y" ]]; then
+      phase_S_cluster_shapes
+  fi
+  phase_M_cluster_microfeatures
+  phase_B_generate_ambiguities
+  make__traineddata
 fi
-phase_M_cluster_microfeatures
-phase_B_generate_ambiguities
-make__traineddata
-
+ 
 tlog "\nCompleted training for language '${LANG_CODE}'\n"
