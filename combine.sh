9a3cd79ed04fb5989375718615888deacb12dcf2
Filleee <62231905+Filleee@users.noreply.github.com>
2021-10-10 12:27:55 +0700

    add combine.sh

---
+++ combine.sh
@@ -0,0 +1,4 @@
+lstmtraining --stop_training \
+	--continue_from data/finetuned_model/_checkpoint \
+	--traineddata data/tess_data/ind.traineddata \
+	--model_output data/finetuned_model/ind.traineddata
