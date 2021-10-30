dc87555fe47605b61570b8706267ba7929e3c003
Filleee <62231905+Filleee@users.noreply.github.com>
2021-10-30 20:19:43 +0700

    change fontlist to arial

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -1,6 +1,6 @@
 rm -rf data/output/*
 ./tesseract/src/training/tesstrain.sh --fonts_dir data/fonts \
-	     --fontlist 'OCR A Extended' \
+	     --fontlist 'Arial' \
 	     --lang ind \
 	     --linedata_only \
 	     --langdata_dir data/lang_data \
