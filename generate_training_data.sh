272ac3b324d55fd6c1381eaae2337867c112980d
Henry Setiawan <henry_setiawan24@yahoo.co.id>
2021-11-29 15:52:51 +0700

    set all sh files to lf

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -1,4 +1,13 @@
 rm -rf data/output/*
-./tesseract/src/training/tesstrain.sh --fonts_dir data/fonts --fontlist 'Arial' --lang ind --linedata_only --langdata_dir data/lang_data --tessdata_dir data/tess_data --save_box_tiff --maxpages 250 --output_dir data/output
-#the max pages will enter the amount of pages to be rendered for training
-#purpose
+./tesseract/src/training/tesstrain.sh --fonts_dir data/fonts \
+	     --fontlist 'Arial' \
+	     --lang ind \
+	     --linedata_only \
+	     --langdata_dir data/lang_data \
+	     --tessdata_dir data/tess_data \
+	     --save_box_tiff \
+	     --maxpages 250 \
+	     --output_dir data/output
+
+#the max pages will enter the amount of pages to be rendered for training purpose
+
