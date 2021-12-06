acbb22a461fc7927067de9b775db2ba11b4b7e50
Henry Setiawan <henry_setiawan24@yahoo.co.id>
2021-12-06 17:40:50 +0700

    added training data for ktp and fake ktp. ind.traineddata is now V0

--- evaluate.sh
+++ evaluate.sh
@@ -7,9 +7,3 @@ lstmeval --model data/tess_data/_checkpoint \
 #--model_dat takes the extracted model as input
 #--traineddata takes the data it was trained on
 #--eval_listfile takes the files that are to be evaluated
-
-lstmeval --model data/tess_data/checkpoints/indV3E.traineddata \
-  --eval_listfile data/output/ind.training_files.txt \
-  --verbosity 2
-
-cd C:/cygwin64/TesseractTrain
