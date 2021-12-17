f05a478a1b113bee38d36ea7ba8eda5cdcfbfd6f
Filleee <62231905+Filleee@users.noreply.github.com>
2021-12-17 22:34:31 +0700

    Delete combine.sh

--- combine.sh
+++
@@ -1,4 +0,0 @@
-lstmtraining --stop_training \
-	--continue_from data/finetuned_model/_checkpoint \
-	--traineddata data/tess_data/ind.traineddata \
-	--model_output data/finetuned_model/ind.traineddata
