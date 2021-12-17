a2305f76358fa39f8ad30ce6afdf72ee79e38a15
Filleee <62231905+Filleee@users.noreply.github.com>
2021-12-17 22:34:33 +0700

    Delete extract_model.sh

--- extract_model.sh
+++
@@ -1,4 +0,0 @@
-combine_tessdata -e data/tess_data/ind.traineddata data/model/ind.lstm
-
-#after flag -e the first path takes the pre-existing data it was trained on
-#the second path specified the output path for the model
