d70e88420ecd710512abb129713ba42d4f9e8b89
Marcel-003 <marcel003@binus.ac.id>
2021-12-17 21:36:17 +0700

    fix hocr

--- hocr.sh
+++ hocr.sh
@@ -4,10 +4,11 @@ lang=ind
 set -- "$SOURCE"*.tif
 for img_file; do
     echo -e  "\r\n File: $img_file"
-    OMP_THREAD_LIMIT=1 tesseract "${img_file}" "${img_file%.*}"  --psm 6  --oem 1  -l $lang -c page_separator='' hocr
-    source training_env/bin/activate
-    PYTHONIOENCODING=UTF-8 hocr-extract-images -b ./myfiles/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
+    OMP_THREAD_LIMIT=1 tesseract --tessdata-dir ./data/tess_data   "${img_file}" "${img_file%.*}"  --psm 6  --oem 1  -l $lang -c page_separator='' hocr
+    source venv/bin/activate
+    PYTHONIOENCODING=UTF-8 ./hocr-extract-images -b ./myfiles/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
     deactivate
 done
 rename s/exp0.txt/exp0.gt.txt/ ./myfiles/*exp0.txt
+
 echo "Image files converted to tif. Correct the ground truth files and then run ocr-d train to create box and lstmf files"
