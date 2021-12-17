8722eb637683d8457fa13a74870a2f344524a97b
Filleee <62231905+Filleee@users.noreply.github.com>
2021-12-17 22:34:32 +0700

    Delete evaluate.sh

--- evaluate.sh
+++
@@ -1,9 +0,0 @@
-lstmeval --model data/tess_data/_checkpoint \
-  --traineddata data/tess_data/ind.traineddata \
-  --eval_listfile data/output/ind.training_files.txt \
-  --verbosity 2
-
-
-#--model_dat takes the extracted model as input
-#--traineddata takes the data it was trained on
-#--eval_listfile takes the files that are to be evaluated
